828e0d594fb591adef6758dd9a573a67cc5952a2
DamienMonchaty <damienmonchaty@hotmail.fr>
2023-06-09 12:35:28 +0200

    test ci/cd

--- src/main/java/com/example/demo/controllers/PersonneRestController.java
+++ src/main/java/com/example/demo/controllers/PersonneRestController.java
@@ -43,8 +43,6 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping(value = "/personnes")
 public class PersonneRestController {
 
-	
-	
 	/** The jms template. */
 	@Autowired
 	JmsTemplate jmsTemplate;
